e908b69a54c869bc24a3b3447107347f116bda1e
FikirMersha <162644747+FikirMersha@users.noreply.github.com>
2024-07-04 12:07:38 -0400

    apple-clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header';
+import Footer from "./component/Footer"
+import Alert from "./component/Alert";
+import FirstSec from "./component/FirstSec"
+import SecondSec from "./component/SecondSec";
+import ThirdSec from "./component/ThirdSec";
+import FourthSec from "./component/FourthSec";
+import FifthSec from "./component/FifthSec"
+import SixthSec from "./component/SixthSec";
+
+import  "./commonResource/css/bootstrap.css"
+import "./commonResource/css/styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Alert />
+      <FirstSec />
+      <SecondSec />
+      <ThirdSec />
+      <FourthSec />
+      <FifthSec />
+      <SixthSec />
+      <Footer />
     </div>
   );
 }
